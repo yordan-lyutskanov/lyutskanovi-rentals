c2d581a38d3f850fc397878f17ec0e0edb60f799
Yordan Lyutskanov <lyutskanov.yordan@gmail.com>
2022-01-06 22:23:10 +0200

    Add menu hiding with animation on scroll

--- js/menu-bar.js
+++ js/menu-bar.js
@@ -2,7 +2,10 @@ let menu = document.getElementById("navbar-items")
 let menuButton = document.getElementById("navbar-menu-button")
 let navbarContainer = document.getElementById("navbar-container")
 const initialMenuHeight = menu.style.maxHeight
+const initialNavbarHeight = navbarContainer.style.maxHeight
 
+
+/* Show dropdown menu on click */
 menuButton.addEventListener("click", ev => {
     console.log(`max height: ${menu.style.maxHeight}`)
     if (menu.style.maxHeight !== "0px" && menu.style.maxHeight){
@@ -15,3 +18,15 @@ menuButton.addEventListener("click", ev => {
         menu.style.maxHeight = "200px"
     }     
 })
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbarContainer.style.top = "0px";
+  } else {
+    navbarContainer.style.top = "-150px";
+}
+  prevScrollpos = currentScrollPos;
+} 
